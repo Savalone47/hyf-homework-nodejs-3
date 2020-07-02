6fe73d9d85024d3c88ac314520d86ddac45dbaec
savalone47 <savalonescott47@gmail.com>
2020-07-02 12:37:57 +0200

    update module

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -11,15 +11,15 @@
 //
 // -- This is a parent command --
 // Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
+
+
+// //-- This is a child command --
 // Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
+
+
+// //-- This is a dual command --
 // Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
+
+
+// //-- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
